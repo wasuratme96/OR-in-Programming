f24599088a191f3809cfa28a8c91e90e411924f6
Wasurat S <wasurat_me96@outlook.com>
2022-09-10 21:33:09 +0700

    Add velocity boundary and linear weight decrease

--- PSO/PSO.m
+++ PSO/PSO.m
@@ -2,82 +2,90 @@ clear all
 close all
 clc
 
-% Define the details of the objective function
-nVar = 10;
-ub = [10 10 10 10 10 10 10 10 10 10]; 
-lb = [-10 -10 -10 -10 -10 -10 -10 -10 -10 -10 ];
-fobj = @ObjectiveFunction;
+% Define the details of the table design problem
+nVar = 10; 
+ub = [10 10 10 10 10 10 10 10 10 10];
+lb = [-10 -10 -10 -10 -10 -10 -10 -10 -10 -10];
+fobj = @ObjectiveFunction; 
 
-% PSO's parameters 
-noP = 300;
-maxIteration = 500;
+% Define the PSO's paramters 
+noP = 30;
+maxIter = 500;
 wMax = 0.9;
 wMin = 0.2;
 c1 = 2;
 c2 = 2;
+vMax = (ub - lb) .* 0.2; 
+vMin  = -vMax;
+
 
 % The PSO algorithm 
 
-% Initialization of particles 
-for k = 1 : noP 
-    Swarm.Particles(k).X = (ub - lb) .* rand(1,nVar) + lb;
-    Swarm.Particles(k).V = zeros(1,nVar);
-    Swarm.Particles(k).PBEST.X = zeros(1, nVar);
-    Swarm.Particles(k).PBEST.O = inf; % for minimization 
+% Initialize the particles 
+for k = 1 : noP
+    Swarm.Particles(k).X = (ub-lb) .* rand(1,nVar) + lb; 
+    Swarm.Particles(k).V = zeros(1, nVar); 
+    Swarm.Particles(k).PBEST.X = zeros(1,nVar); 
+    Swarm.Particles(k).PBEST.O = inf; 
+    
+    Swarm.GBEST.X = zeros(1,nVar);
+    Swarm.GBEST.O = inf;
 end
 
-Swarm.GBEST.X = zeros(1,nVar);
-Swarm.GBEST.O = inf;
 
-% main loop 
-for t = 1 : maxIteration 
+% Main loop
+for t = 1 : maxIter
     
+    % Calcualte the objective value
     for k = 1 : noP
         currentX = Swarm.Particles(k).X;
-        currentO = fobj ( currentX );
+        Swarm.Particles(k).O = fobj(currentX);
         
-        if currentO <  Swarm.Particles(k).PBEST.O  % Update PBEST
+        % Update the PBEST
+        if Swarm.Particles(k).O < Swarm.Particles(k).PBEST.O 
             Swarm.Particles(k).PBEST.X = currentX;
-            Swarm.Particles(k).PBEST.O = currentO;
+            Swarm.Particles(k).PBEST.O = Swarm.Particles(k).O;
         end
         
-        if  currentO < Swarm.GBEST.O   % Update GBEST
+        % Update the GBEST
+        if Swarm.Particles(k).O < Swarm.GBEST.O
             Swarm.GBEST.X = currentX;
-            Swarm.GBEST.O = currentO;
-        end     
+            Swarm.GBEST.O = Swarm.Particles(k).O;
+        end
     end
     
-    w = wMax - t .* ( (wMax - wMin) / maxIteration);
+    % Update the X and V vectors 
+    w = wMax - t .* ((wMax - wMin) / maxIter);
     
     for k = 1 : noP
-        Swarm.Particles(k).V = w .* Swarm.Particles(k).V + c1 .* rand(1, nVar) .* ( Swarm.Particles(k).PBEST.X - Swarm.Particles(k).X) ...
-                                                                                     + c2 .* rand(1,nVar) .*  (Swarm.GBEST.X - Swarm.Particles(k).X) ;     
-        Swarm.Particles(k).X  = Swarm.Particles(k).X + Swarm.Particles(k).V;                                                                 
+        Swarm.Particles(k).V = w .* Swarm.Particles(k).V + c1 .* rand(1,nVar) .* (Swarm.Particles(k).PBEST.X - Swarm.Particles(k).X) ...
+                                                                                     + c2 .* rand(1,nVar) .* (Swarm.GBEST.X - Swarm.Particles(k).X);
                                                                                  
+        
+        % Check velocities 
+        index1 = find(Swarm.Particles(k).V > vMax);
+        index2 = find(Swarm.Particles(k).V < vMin);
+        
+        Swarm.Particles(k).V(index1) = vMax(index1);
+        Swarm.Particles(k).V(index2) = vMin(index2);
+        
+        Swarm.Particles(k).X = Swarm.Particles(k).X + Swarm.Particles(k).V;
+        
+        % Check positions 
+        index1 = find(Swarm.Particles(k).X > ub);
+        index2 = find(Swarm.Particles(k).X < lb);
+        
+        Swarm.Particles(k).X(index1) = ub(index1);
+        Swarm.Particles(k).X(index2) = lb(index2);
+        
     end
-    disp(Swarm.GBEST.O)
+    
+    outmsg = ['Iteration# ', num2str(t) , ' Swarm.GBEST.O = ' , num2str(Swarm.GBEST.O)];
+    disp(outmsg);
     
     cgCurve(t) = Swarm.GBEST.O;
-    
 end
-
-semilogy(cgCurve)
-    
-
-
-
-
-
-
-
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-
+ 
+semilogy(cgCurve);
+xlabel('Iteration#')
+ylabel('Weight')
